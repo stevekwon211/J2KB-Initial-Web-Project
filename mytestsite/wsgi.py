f3e53b050c174fefe88361e81d40694579130707
Hyukil Kwon <stevekwon211@gmail.com>
2020-06-09 18:15:00 +0900

    .

---
+++ mytestsite/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mytestsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os, sys
+from django.core.wsgi import get_wsgi_application
+
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
+application = get_wsgi_application()
